5e53f557229a5479b4b3113fc21d5c7853f2b87d
Fuse <firebom.sns@gmail.com>
2018-04-11 14:11:46 +0000

    sanin

--- app/controllers/sanin_reviews_controller.rb
+++ app/controllers/sanin_reviews_controller.rb
@@ -1,4 +1,4 @@
-class SaninReviewsController < ApplicationControllerApplicationController
+class SaninReviewsController < ApplicationController
   before_action :require_user_logged_in, only: [:new, :crete, :destroy] 
   before_action :correct_user, only: [:destroy] 
   
